b827b3a963a58c697bf9ae71bed9b821b7a8bccc
Diogo Frazao <doga.frazao@gmail.com>
2020-04-08 21:13:21 +0100

    projeto student e dashboard - parte 2

---
+++ app/SubjectEnrollment.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SubjectEnrollment extends Model
+{
+    protected $table = 'subjectEnrollments';
+    protected $primaryKey = 'idUser';
+}
