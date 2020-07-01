03a3954a9b8ff24f3c057926c2208f4f779ce834
joaom <joao.migueltp97@gmail.com>
2020-07-02 00:37:59 +0100

    help erro corrigido

--- resources/lang/pt/gx.php
+++ resources/lang/pt/gx.php
@@ -471,7 +471,7 @@ return [
                         disponibilidade dos outros membros dos grupos. Outra funcionalidade é a criação de uma reunião com os membros do grupo, especifando a descrição o sítio e a data da mesma.',
 
     'helpProjectStud3' => 'Nesta área o utilizador consegue aceder ao fórum do projeto, ver publicações de outros estudante e professores , conseguindo criar publicações com um título e um corpo de texto.',
-    'helpProjectStud4' => 'Nesta área o utilizador consegue submeter ficheiros do grupo para avaliação, e avaliar a participação de cada membro do grupo no desenvolviemtno do projeto (escala: 1-5 estrelas) 1 estrela : nota mínima , 5 estrelas : nota máxima.',
+    'helpProjectStud4' => 'Nesta área o utilizador consegue observar a informação da submissão e se possível mudar os ficheiros submetidos(apagando) , e avaliar a participação de cada membro do grupo no desenvolviemtno do projeto (escala: 1-5 estrelas) 1 estrela : nota mínima , 5 estrelas : nota máxima.',
 
     'helpGroupStud' => 'Ao clicar neste botão, um modal será exibido e o utilizador poderá criar um grupo de trabalho para o projeto , o utilizador pode adicionar
     outros estudantes que pertençam à mesma unidade curricular que ainda não tenho grupo até ao momento, ou o utilizador poderá criar um grupo apenas com um elemento(grupo apenas com o utilizador).Quando o utilizador cria o grupo , será redirecionado para a página do projeto.
